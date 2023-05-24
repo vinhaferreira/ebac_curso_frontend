ba66e783ad374806a07f423ba554b8d1f51603fe
Flávia Ferreira <vinhaferreira@gmail.com>
2023-05-24 15:52:29 -0300

    Exercicio do Modulo 07

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("myForm");
+const messageDiv = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const numA = Number(document.getElementById("numA").value);
+  const numB = Number(document.getElementById("numB").value);
+
+  if (numB > numA) {
+    messageDiv.textContent = "O Formulário é válido!";
+    messageDiv.style.color = "green";
+  } else {
+    messageDiv.textContent = "O número B deve ser maior que o número A";
+    messageDiv.style.color = "red";
+  }
+
+});
